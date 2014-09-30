e46fedf1ad7a4fd7b8d64e335485a231234a87d4
Duncan Beaton <dunckr@gmail.com>
2014-09-30 22:37:43 +0100

    init

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+notesPath="/Applications/Notes.app/Contents/Resources/"
+
+link() {
+    from="$1"
+    to="$2"
+    echo "Linking '$from' to '$to'"
+    rm -f "$to"
+    ln -s "$from" "$to"
+}
+
